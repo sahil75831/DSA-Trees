2d084f8513b997bc8cfc7794b3e2ec5fd9d1785a
sahilsharma <sahilsharma75831@gmail.com>
2023-07-28 14:59:31 +0530

    first commit

--- dsa.js
+++ dsa.js
@@ -1,48 +1,54 @@
-// Given two binary trees, the task is to find if both of them are identical or not. 
+// // // Given a Binary Tree, convert it into its mirror.
 
-class Node {
-    constructor(data) {
-      this.data = data;
-      this.left = null;
-      this.right = null;
+class TreeNode {
+    constructor(val) {
+      this.val = val;
+      this.left = this.right = null;
     }
   }
-
-// Example usage:
-// Create 1 binary tree:
-//       1
-//      / \
-//     2   3
-//    / \
-//   4   5
-
-// Create 2 binary tree:
-//       1
-//      / \
-//     2   3
-//    / \
-//   14   5
-function areTreesIdentical(root1, root2){
-    if(root1===null && root2===null) return true
-    if((root1===null && root2!==null) || (root1!==null && root2===null)) return false
-    if(root1.data !== root2.data) return false
-    return (areTreesIdentical(root1.left, root2.left) && areTreesIdentical(root1.right, root2.right))
-    
-}
-
-// Create the first binary tree
-const root1 = new Node(1);
-root1.left = new Node(2);
-root1.right = new Node(3);
-root1.left.left = new Node(4);
-root1.left.right = new Node(5);
-
-// Create the second binary tree
-const root2 = new Node(1);
-root2.left = new Node(2);
-root2.right = new Node(3);
-root2.left.left = new Node(14);
-root2.left.right = new Node(5);
-
-const result = areTreesIdentical(root1, root2);
-console.log(result); // Output: false
+  
+  function mirrorBinaryTree(root) {
+    if (!root) {
+      return null;
+    }
+  
+    // Swap the left and right subtrees
+    [root.left, root.right] = [root.right, root.left];
+  
+    // Recursively convert left and right subtrees
+    mirrorBinaryTree(root.left);
+    mirrorBinaryTree(root.right);
+  
+    return root;
+  }
+  
+  // Helper function to print the binary tree (for visualization)
+  function printBinaryTree(root) {
+    if (!root) {
+      return;
+    }
+  
+    console.log(root.val);
+    printBinaryTree(root.left);
+    printBinaryTree(root.right);
+  }
+  
+  // Example usage:
+  // Create a sample binary tree
+  const root = new TreeNode(1);
+  root.left = new TreeNode(2);
+  root.right = new TreeNode(3);
+  root.left.left = new TreeNode(4);
+  root.left.right = new TreeNode(5);
+  
+  // Print the original binary tree
+  console.log("Original binary tree:");
+  printBinaryTree(root);
+  
+  // Convert the binary tree to its mirror
+  mirrorBinaryTree(root);
+  
+  // Print the mirrored binary tree
+  console.log("\nMirrored binary tree:");
+  printBinaryTree(root); // 1 3 2 5 4 
+  
